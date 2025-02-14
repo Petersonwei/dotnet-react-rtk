503d65ccc5adf068d316ea54a0d4749f958345f5
petersonwei <weipetersonwei@gmail.com>
2025-02-14 11:14:05 +1000

    Commented out the unused api module in Program.cs

--- Restore/API/Program.cs
+++ Restore/API/Program.cs
@@ -3,22 +3,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// remove this, not needed for now, it is the alternative of swagger
+// builder.Services.AddOpenApi(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+// remove this for now, will be used in future
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
