fd317ed48eae0bd187dd95d2309f288dfce237cc
petersonwei <weipetersonwei@gmail.com>
2025-02-17 10:29:24 +1000

    add CORS middleware to .NET backend

--- Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aecff67e9a747d52d6ef36f9b8b0a031a1de5bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2f8bc0a14c6eae6635f3aa0d14c0aa42353143")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
