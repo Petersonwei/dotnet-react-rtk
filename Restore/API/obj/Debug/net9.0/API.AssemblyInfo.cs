77de40e27dff8759d2cd935f65b776050cc13ec1
petersonwei <weipetersonwei@gmail.com>
2025-02-17 15:30:40 +1000

    Adding loading indicators

--- Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a0186964ac6e8d45639ff07efbca625f481da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1b72489bf3a40ca77e0ffa351842d8471d22ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Restore/client/src/app/layout/NavBar.tsx
+++ Restore/client/src/app/layout/NavBar.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, LinearProgress, List, ListItem, Toolbar, Typography } from "@mui/material";
 import { DarkMode, LightMode, ShoppingCart } from '@mui/icons-material';
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "../store/store";
 
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
@@ -31,6 +32,9 @@ type Props = {
 }
 
 export default function NavBar({ darkMode, toggleDarkMode }: Props) {
+
+    const { isLoading } = useAppSelector(state => state.ui);
+
     return (
         <AppBar position="fixed">
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -76,6 +80,11 @@ export default function NavBar({ darkMode, toggleDarkMode }: Props) {
                 </Box>
 
             </Toolbar>
+            { isLoading && (
+            <Box sx={{ width: '100%'}}>
+                 <LinearProgress color="secondary"/> 
+            </Box>
+            )}
         </AppBar>
     )
 }
